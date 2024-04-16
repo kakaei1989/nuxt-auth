23dfbbc8c0585943270915db1c7a23c26f2f7bd2
Ahmad Kakaei <kakaei1989@gmail.com>
2024-04-16 21:40:22 +0330

    add logout api

--- components/Header.vue
+++ components/Header.vue
@@ -22,16 +22,31 @@
                         <a class="nav-link" href="#">Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
+                        <a class="nav-link" @click="logout" href="#">Logout</a>
                     </li>
                     <li class="nav-item">
                         <NuxtLink class="nav-link" activeClass="active" to="/auth/register">Register</NuxtLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
+                        <NuxtLink class="nav-link" activeClass="active" to="/auth/login">Login</NuxtLink>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 </template>
+
+<script setup>
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+async function logout() {
+    await useFetch('/api/auth/logout', {
+        method: 'POST'
+    });
+
+    toast.warning("You are logouted!");
+    return navigateTo('/')
+}
+</script>
