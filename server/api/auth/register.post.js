612236a6c592ca8a9efba7c8aa4e3594b5fcf8ed
Ahmad Kakaei <kakaei1989@gmail.com>
2024-04-16 20:11:45 +0330

    add internal register api

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -4,18 +4,21 @@ export default defineEventHandler(async (event) => {
     try {
         const data = await $fetch('http://localhost:8000/api/register' , {
             method: 'POST',
-            body: 'body',
+            body: body,
             headers: {
                 'Accept': 'application/json',
             }
         })
 
-        console.log(data);
-        return data;
+        setCookie(event, 'token', data.token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 60 * 60 * 24 * 7, // 1 week
+            path: '/'
+        })
+
+        return data.user;
     } catch (error) {
-        console.log(error);
         return error
     }
-
-    return body
 })
